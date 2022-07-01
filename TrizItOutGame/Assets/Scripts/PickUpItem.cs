6793b98e4613369530e5a4ac876ca70ec95d3340
TomBer101 <74832275+TomBer101@users.noreply.github.com>
2022-07-01 17:49:13 +0300

    PickUpItem is functional. We need to fix the proportion of an image inside the inventory (I think we should make a smaller pic) not done - need to show only relevant items.

--- TrizItOutGame/Assets/Scripts/PickUpItem.cs
+++ TrizItOutGame/Assets/Scripts/PickUpItem.cs
@@ -1,18 +1,44 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
-public class PickUpItem : MonoBehaviour
+public class PickUpItem : MonoBehaviour, IInteractable
 {
-    // Start is called before the first frame update
+    [SerializeField]
+    private string m_DisplaySprite; // We need to know wich sprite it should active with.
+
+    private GameObject m_InventorySlots;
+
+
+   
     void Start()
+    {
+        m_InventorySlots = GameObject.Find("Items_Parent"); // To loop up for the available slot.
+    }
+
+ 
+    void Update()
     {
         
     }
 
-    // Update is called once per frame
-    void Update()
+    public void Interact(DisplayManagerLevel1 currDisplay)
     {
-        
+        ItemPickUp();
+    }
+
+    private void ItemPickUp()
+    {
+        Debug.Log("Got into Pick Up Item function. Item name: " + m_DisplaySprite);
+        foreach(Transform slot in m_InventorySlots.transform)
+        {
+            if(slot.transform.GetChild(0).GetComponent<Image>().sprite.name == "empty_item")
+            {
+                slot.transform.GetChild(0).GetComponent<Image>().sprite = Resources.Load<Sprite>("Sprites/Inventory/" + m_DisplaySprite);
+                Destroy(gameObject);
+                break;
+            }
+        }
     }
 }

cf4f33b5b87e76150cf1b8f5ad3da3b4176fb860
tomSapir <70592152+tomSapir@users.noreply.github.com>
2022-07-23 12:45:26 +0300

    start working

---
+++ TrizItOutGame/Assets/Scripts/HintsManager.cs
@@ -0,0 +1,60 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HintsManager : MonoBehaviour
+{
+    private Dictionary<int, string> m_HintsMap;
+    private int m_CurrentHintIndex;
+
+    [SerializeField]
+    private GameObject m_HintWindow;
+    [SerializeField]
+    private GameObject m_HintBtn;
+    [SerializeField]
+    private TMPro.TextMeshPro m_HintText;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        m_HintsMap = new Dictionary<int, string>();
+        m_CurrentHintIndex = 0;
+        setHintsMapData();
+
+        // TODO: add all the hints to "Hints"
+    }
+
+    private void setHintsMapData()
+    {
+        // TODO: this method will fill the map with the hints and there indexes
+        m_HintsMap.Add(0, "test for checking.");
+    }
+
+    public void OnClickHintBtn()
+    {
+        if (m_HintsMap.ContainsKey(m_CurrentHintIndex))
+        {
+            m_HintWindow.SetActive(true);
+            m_HintBtn.SetActive(false);
+
+            // get the current hint from Hints map
+            string currentHint = m_HintsMap[m_CurrentHintIndex];
+
+            // TODO: put inside the text
+            m_HintText.text = currentHint;
+           
+
+           
+        }
+        else
+        {
+            Debug.LogError("There is no hint with index " + m_CurrentHintIndex + " In the hints map.");
+        }
+    }
+
+    public void OnClickCloseHintWindow()
+    {
+        m_HintWindow.SetActive(false);
+        m_HintBtn.SetActive(true);
+    }
+}

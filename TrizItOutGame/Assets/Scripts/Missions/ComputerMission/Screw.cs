1a03ad2a2a702c9ba4c03a9910d030192bb86210
Eden Sofir <edensofir2710@gmail.com>
2022-08-16 14:27:16 +0300

    Note removed when intract with screw
    Note removed when intract with screw

--- TrizItOutGame/Assets/Scripts/Missions/ComputerMission/Screw.cs
+++ TrizItOutGame/Assets/Scripts/Missions/ComputerMission/Screw.cs
@@ -8,23 +8,27 @@ public class Screw : MonoBehaviour, IInteractable
     [SerializeField]
     private string m_UnlockItem;
     private string m_UnlockItem2 = "trizCoin";
-    private GameObject m_inventory;
+    private GameObject m_Inventory;
 
     // Start is called before the first frame update
     void Start()
     {
-        m_inventory = GameObject.Find("Inventory");
+        m_Inventory = GameObject.Find("Inventory");
     }
 
     public void Interact(DisplayManagerLevel1 currDisplay)
     {
-        string name = m_inventory.GetComponent<InventoryManager>().m_currentSelectedSlot.gameObject.transform.GetChild(0).GetComponent<Image>().sprite.name;
+        string name = m_Inventory.GetComponent<InventoryManager>().m_currentSelectedSlot.gameObject.transform.GetChild(0).GetComponent<Image>().sprite.name;
 
         if (name == m_UnlockItem || name == m_UnlockItem2)
         {
+            if(name == m_UnlockItem2)
+            {
+                m_Inventory.GetComponent<InventoryManager>().RemoveFromInventory("Note");
+            }
             SoundManager.PlaySound(SoundManager.k_ScrewOpenSoundName);
             Destroy(gameObject);
-            m_inventory.GetComponent<InventoryManager>().m_currentSelectedSlot.GetComponent<SlotManager>().ClearSlot();
+            m_Inventory.GetComponent<InventoryManager>().m_currentSelectedSlot.GetComponent<SlotManager>().ClearSlot();
         }
 
     }

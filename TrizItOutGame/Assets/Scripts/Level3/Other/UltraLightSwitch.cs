924a11bdb11f2c534d79e17d5b67bb684804b4bb
TomBer101 <74832275+TomBer101@users.noreply.github.com>
2022-08-26 18:18:13 +0300

    UltraViolet
    Working. Think we should add more lines of pictures (more buttons for the mission).

---
+++ TrizItOutGame/Assets/Scripts/Level3/Other/UltraLightSwitch.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UltraLightSwitch : MonoBehaviour, IInteractable
+{
+    public Sprite m_SwitchOnSprite;
+    public Sprite m_SwitchOffSprite;
+    public GameObject m_UltraVioletLighting;
+
+    private bool m_IsSwitchOn = false;
+
+    public void Interact(DisplayManagerLevel1 currDisplay)
+    {
+        SoundManager.PlaySound(SoundManager.k_SwitchSoundName);
+        m_IsSwitchOn = !m_IsSwitchOn;
+        GetComponent<SpriteRenderer>().sprite = m_IsSwitchOn is true ? m_SwitchOnSprite : m_SwitchOffSprite;
+        m_UltraVioletLighting.GetComponent<SpriteRenderer>().enabled = m_IsSwitchOn;
+
+        for(int i = 0; i < m_UltraVioletLighting.transform.childCount; i++)
+        {
+            m_UltraVioletLighting.transform.GetChild(i).gameObject.GetComponent<SpriteRenderer>().enabled = m_IsSwitchOn;
+        }
+    }
+}

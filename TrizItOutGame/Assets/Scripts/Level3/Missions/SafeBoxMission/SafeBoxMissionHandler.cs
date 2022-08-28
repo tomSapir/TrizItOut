e8423a04434f55950afda9394e11e21c5f2b8caa
Eden Sofir <edensofir2710@gmail.com>
2022-08-28 12:48:30 +0300

    safrbox commit

--- TrizItOutGame/Assets/Scripts/Level3/Missions/SafeBoxMission/SafeBoxMissionHandler.cs
+++ TrizItOutGame/Assets/Scripts/Level3/Missions/SafeBoxMission/SafeBoxMissionHandler.cs
@@ -3,40 +3,49 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.EventSystems;
-    public class SafeBoxMissionHandler : MonoBehaviour
+public class SafeBoxMissionHandler : MonoBehaviour
 {
     private HashSet<int> m_currentSolution = new HashSet<int>();
-    private readonly HashSet<int> m_Solution = new HashSet<int>() { };
+    private readonly HashSet<int> m_Solution = new HashSet<int>() {1,7,8,11,12,3};
 
-    public GameObject indicator;
+    public delegate void SafeBoxOpenedDelegate();
+    public event SafeBoxOpenedDelegate SafeBoxOpened;
+
+    public GameObject m_Indicator;
+    public Sprite m_SafeBoxOpened;
+
     void Start()
     {
-        
+
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+
     }
 
     public void OnClick()
     {
         GameObject button = EventSystem.current.currentSelectedGameObject;
         Image image = button.GetComponent<Button>().image;
-        image.color = new Color32(217, 91, 255,152);
+        image.color = new Color32(217, 91, 255, 152);
         m_currentSolution.Add(int.Parse(button.name));
     }
 
-    private void submitSolution()
+    public void SubmitSolution()
     {
-        if(m_currentSolution == m_Solution)
+        Debug.Log("enterd");
+        if (m_currentSolution == m_Solution)
         {
-            indicator.GetComponent<Image>().color = new Color32(26, 97, 0, 255);
+            m_Indicator.GetComponent<Image>().color = new Color32(12, 255, 0, 255);
+            GameObject.Find("SafeBoxMission").GetComponent<SpriteRenderer>().sprite = m_SafeBoxOpened;
+            SafeBoxOpened?.Invoke();
         }
         else
         {
-            indicator.GetComponent<Image>().color = new Color32(203, 0, 0, 163);
+            m_Indicator.GetComponent<Image>().color = new Color32(255, 0, 71, 255);
+            m_currentSolution.Clear();
+            //TODO: TRASFORM ALL BUTTONS COLORS TO WHITE AGAIN
         }
     }
 }

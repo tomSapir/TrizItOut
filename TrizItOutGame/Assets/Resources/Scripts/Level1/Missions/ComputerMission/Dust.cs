5d54d25b1152bb570567025b6a300f78b110e4b2
tomSapir <70592152+tomSapir@users.noreply.github.com>
2022-09-04 14:22:07 +0300

    end working with eden

--- TrizItOutGame/Assets/Resources/Scripts/Level1/Missions/ComputerMission/Dust.cs
+++ TrizItOutGame/Assets/Resources/Scripts/Level1/Missions/ComputerMission/Dust.cs
@@ -7,7 +7,8 @@ public class Dust : MonoBehaviour, IInteractable
 {
     public delegate void CleanDustAction();
 
-    private string m_UnlockItem = "Spray_With_Straw";
+    private string m_UnlockItem1 = "Spray_With_Straw";
+    private string m_UnlockItem2 = "Broom";
     private GameObject m_inventory;
 
     public event CleanDustAction OnCleanUp;
@@ -20,13 +21,23 @@ public class Dust : MonoBehaviour, IInteractable
 
     public void Interact(DisplayManagerLevel1 currDisplay)
     {
-        // suppose we have a key that unlocks this drawer, the sprites name of the kwy would be m_unlocks.
-        if (m_inventory.GetComponent<InventoryManager>().m_CurrentSelectedSlot.gameObject.transform.GetChild(0).GetComponent<Image>().sprite.name == m_UnlockItem)
+        GameObject currentSelectedSlot = m_inventory.GetComponent<InventoryManager>().m_CurrentSelectedSlot;
+
+        if (currentSelectedSlot != null)
         {
-            SoundManager.PlaySound(SoundManager.k_SpraySoundName);
-            Destroy(gameObject);
-            OnCleanUp?.Invoke();
-            m_inventory.GetComponent<InventoryManager>().m_CurrentSelectedSlot.GetComponent<SlotManager>().ClearSlot();
+            string selectedSlotSpriteName = currentSelectedSlot.gameObject.transform.GetChild(0).GetComponent<Image>().sprite.name;
+
+            if (selectedSlotSpriteName == m_UnlockItem1 || selectedSlotSpriteName == m_UnlockItem2)
+            {
+                if (selectedSlotSpriteName == m_UnlockItem1)
+                {
+                    SoundManager.PlaySound(SoundManager.k_SpraySoundName);
+                }
+
+                Destroy(gameObject);
+                OnCleanUp?.Invoke();
+                m_inventory.GetComponent<InventoryManager>().m_CurrentSelectedSlot.GetComponent<SlotManager>().ClearSlot();
+            }
         }
     }
 }

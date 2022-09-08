2ee8d7362b8a98b99da2468db313ec49e72e9bce
Tom Sapir <tomsp@mta.ac.il>
2022-09-08 10:15:23 +0300

    Fix magnifying glass bug

---
+++ TrizItOutGame/Assets/Resources/Scripts/AllLevels/Inventory/MagnifyingGlassManager.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MagnifyingGlassManager : MonoBehaviour
+{
+    public Sprite m_ZoomBtnHighlightSprite;
+    public Sprite m_ZoomBtnNormaltSprite;
+    public GameObject m_ZoomBtn;
+
+    void Start()
+    {
+        
+    }
+
+
+    void Update()
+    {
+        
+    }
+
+    public void HighlightWhenEnter()
+    {
+        m_ZoomBtn.GetComponent<Image>().sprite = m_ZoomBtnHighlightSprite;
+    }
+
+    public void RemoveHighlightWhenEnter()
+    {
+        m_ZoomBtn.GetComponent<Image>().sprite = m_ZoomBtnNormaltSprite;
+    }
+}

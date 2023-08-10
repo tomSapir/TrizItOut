abd66bfc73696eb46b6de666ba08f755f94b79f2
Yael <yaelbarak6@gmail.com>
2023-08-10 13:46:09 +0300

    Sole buds in level3

--- TrizItOutGame/Assets/Resources/Sprites/Hitcode/script/system/SaveLoad/Saveable.cs
+++
@@ -1,9 +0,0 @@
-﻿using UnityEngine;
-namespace Hitcode_RoomEscape
-{
-    public abstract class Saveable : MonoBehaviour
-    {
-        public abstract SaveInfo Save();
-        public abstract void Load(SaveInfo saveInfo);
-    }
-}
